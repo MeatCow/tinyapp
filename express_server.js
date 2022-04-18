0f7ce9ec0a639a70f53fa1ae93c887d2b91630d8
Matthieu Pauze <matt.pauze@gmail.com>
2022-04-18 17:02:36 -0400

    Removed EJS practice

--- express_server.js
+++ express_server.js
@@ -4,20 +4,9 @@ let app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  const mascots = [
-    {name: 'Sammy', organization: 'DigitalOcean', birthYear: 2012},
-    {name: 'Tux', organization: 'Linux', birthYear: 1996},
-    {name: 'Moby Dock', organization: 'Docker', birthYear: 2013},
-  ];
-  const tagline = 'No programming concept is complete without a cute animal mascot.';
-  res.render('pages/index', {
-    mascots,
-    tagline
-  });
 });
 
 app.get('/about', function(req, res) {
-  res.render('pages/about');
 });
 
 app.listen(8080);
