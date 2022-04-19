c737898bcc40f86cb856ebad43cf932cf5540d1d
Matthieu Pauze <matt.pauze@gmail.com>
2022-04-18 22:46:20 -0400

    Added utils.js file with randomID function Updated routes to point to ejs.

--- express_server.js
+++ express_server.js
@@ -1,5 +1,8 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const { generateRandomString } = require('./lib/utils');
+
+const app = express();
 const PORT = 8080;
 
 const urlDatabase = {
@@ -8,22 +11,45 @@ const urlDatabase = {
 };
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  if (!longURL) {
+    return res.status(404).send("Resource does not exists.");
+  }
+  res.redirect(longURL);
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render('urls_index.ejs', templateVars);
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
-app.get("/url/:shortURL", (req, res) => {
+app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
-  res.render('urls_show.ejs', templateVars);
+  res.render('urls_show', templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
+app.get("*", (req, res) => {
+  res.redirect("/urls/new");
+});
+
+app.post("/urls/new", (req, res) => {
+  const newKey = generateRandomString(6);
+  let longURL = req.body.longURL;
+  if (!longURL.includes("http://")) {
+    longURL = "http://" + longURL;
+  }
+  urlDatabase[newKey] = longURL;
+  res.redirect(303, `/urls/${newKey}`);
 });
 
 app.listen(PORT, () => {
