922b18af6a4d29068d7aa83233364f4fed1bf647
Matthieu Pauze <matt.pauze@gmail.com>
2022-04-21 00:06:34 -0400

    Only logged in users can shorten urls. Updated structure of urlDatabase to track owner.

--- express_server.js
+++ express_server.js
@@ -7,8 +7,14 @@ const app = express();
 const PORT = 8080;
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userId: "Nl6XyH"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userId: "Nl6XyH"
+  }
 };
 
 const usersDatabase = {
@@ -43,6 +49,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+//TODO: Replace with redirects with error codes. Maybe Ajax?
 const renderError = (req, res, errMsg, errorCode) => {
   const user = usersDatabase.findById(req.cookies.userId);
   console.log("ID:", req.body.id, "User:", user);
@@ -51,8 +58,12 @@ const renderError = (req, res, errMsg, errorCode) => {
   });
 };
 
+const isLoggedIn = (request) => {
+  return !!request.cookies.userId;
+};
+
 app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
+  let longURL = urlDatabase[req.params.shortURL].longURL;
   if (!longURL) {
     return renderError(req, res, "Resource does not exists", 404);
   }
@@ -60,6 +71,9 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
+  if (!isLoggedIn(req)) {
+    return res.redirect("/urls");
+  }
   const user = usersDatabase.findById(req.cookies.userId);
   const templateVars = {
     user
@@ -71,7 +85,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const user = usersDatabase.findById(req.cookies.userId);
   const templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL],
+    longURL: urlDatabase[req.params.shortURL].longURL,
     user
   };
   res.render('urls_show', templateVars);
@@ -87,6 +101,9 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
+  if (isLoggedIn(req)) {
+    return res.redirect("/urls");
+  }
   const templateVars = {
     user: usersDatabase.findById(req.cookies.userId)
   };
@@ -94,6 +111,9 @@ app.get("/register", (req, res) => {
 });
 
 app.get('/login', (req, res) => {
+  if (isLoggedIn(req)) {
+    return res.redirect("/urls");
+  }
   const templateVars = {
     user: usersDatabase.findById(req.cookies.userId)
   };
@@ -101,34 +121,49 @@ app.get('/login', (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.redirect("/urls/new");
+  res.redirect("/urls");
 });
 
 app.post("/urls/new", (req, res) => {
+  if (!isLoggedIn(req)) {
+    return res.redirect("/urls");
+  }
   let newKey = generateRandomString(6, usersDatabase.users);
   
   let longURL = req.body.longURL;
   if (!longURL.includes("http://")) {
     longURL = "http://" + longURL;
   }
-  urlDatabase[newKey] = longURL;
+  urlDatabase[newKey] = {
+    longURL,
+    userId: req.cookies.userId
+  };
   
   res.redirect(303, `/urls/${newKey}`);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
+  if (!isLoggedIn(req)) {
+    return res.redirect("/urls");
+  }
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL", (req, res) => {
+  if (!isLoggedIn(req)) {
+    return res.redirect("/urls");
+  }
   const shortURL = req.params.shortURL;
   const newURL = req.body.newURL;
-  urlDatabase[shortURL] = newURL;
+  urlDatabase[shortURL].longURL = newURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
 app.post("/login", (req, res) => {
+  if (isLoggedIn(req)) {
+    return res.redirect("/urls");
+  }
   const user = usersDatabase.findByEmail(req.body.email);
   const password = req.body.password;
 
@@ -146,6 +181,9 @@ app.post("/logout", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
+  if (isLoggedIn(req)) {
+    return res.redirect("/urls");
+  }
   const email = req.body.email;
   const password = req.body.password;
 
